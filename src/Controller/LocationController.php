ea7bf24a05d4a3a3c51537d5b1c85be44fe29fd8
ssole <saul.sole@gmail.com>
2026-06-15 02:01:12 +0200

    v0.1.11 Added translations on location

---
+++ src/Controller/LocationController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/location/')]
+final class LocationController extends AbstractController
+{
+    #[Route('how-to-arrive', name: 'app_info_transport', methods: ['GET'])]
+    public function main(): Response
+    {
+        return new Response('TODO');
+    }
+}
